847fff8ae945e087c6bf29f78d7bbd64a1dfe00f
한지희 <g2hhh2ee@gmail.com>
2022-05-11 00:00:55 +0900

    Set: set project

---
+++ projects/Typescript/todos/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "plugin:react/recommended",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
