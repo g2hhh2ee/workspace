fd4da8597b3cd90550c1d5836ebabbcc45590626
한지희 <g2hhh2ee@gmail.com>
2022-05-21 20:07:08 +0900

    Set: set project

---
+++ projects/Typescript/nextjs-ts-tailwind_플젝세팅_더자세한거/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    //기본 폰트로 사용할 폰트들 기본설정해줌.
+    fontFamily: {
+      sans: ["Noto Sans KR", "Montserrat"],
+      serif: ["Lato"],
+      mono: ["Corinthia", "Shadows Into Light", "Staatliches"],
+      cursive: ["Architects Daughter"],
+      jua: ["Jua"],
+    },
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide"),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  variants: {
+    extend: {},
+  },
+};
---
+++ projects/gram/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>instagram 2.0 build</h1>
+    </div>
+  )
+}
+
+export default Home
