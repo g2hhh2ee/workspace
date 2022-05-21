3028b96117d86f539efbd35d64394b81c0caba6a
한지희 <g2hhh2ee@gmail.com>
2022-05-21 20:28:06 +0900

    Set: convert to js

---
+++ projects/gram/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>hihi</h1>
+    </div>
+  )
+}
+
+export default Home
